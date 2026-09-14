8b452938cd5525437a1963855e7f8c45fa2bffa1
Alexandra_08 <alexandramizero@gmail.com>
2026-09-15 00:42:05 +0200

    refactor: move validate game payload function to validation file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// @ts-expect-error type error without @types/node package
 import process from "node:process";
+import path from 'path';
+
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
@@ -11,14 +12,14 @@ export default defineConfig(() => ({
     react(),
     tailwindcss(),
   ],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 3000,
+    port: 3000, // specify a fixed port
     strictPort: true,
     host: host || false,
     hmr: host
@@ -29,7 +30,6 @@ export default defineConfig(() => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
