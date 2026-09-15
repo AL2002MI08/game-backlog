227a0284a5aed8d8a359ae0d9b3c75a49fef3f34
Alexandra_08 <alexandramizero@gmail.com>
2026-09-15 11:36:35 +0200

    feat: add custom components and navbar and use absolute paths for routes

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { RouterProvider } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { theme } from "@/theme";
+import { router } from "@/route";
+import "@mantine/core/styles.css";
+import "./App.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 1000 * 60 * 2,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme} defaultColorScheme="dark">
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
--- src/route.tsx
+++ src/route.tsx
@@ -1,10 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import RootLayout from "./layouts/RootLayout";
-import Games from "./pages/Games";
-import GameDetails from "./pages/GameDetail";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-
+import RootLayout from "@/layouts/RootLayout";
+import Games from "@/pages/Games";
+import GameDetails from "@/pages/GameDetail";
+import Login from "@/pages/Login";
+import NotFound from "@/pages/NotFound";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -16,10 +15,11 @@ export const router = createBrowserRouter([
   },
   {
     element: <RootLayout />,
+
     children: [
       {
         path: "/games",
-        element: <Games />,
+        element: <Games />
       },
       {
         path: "/games/:id",
